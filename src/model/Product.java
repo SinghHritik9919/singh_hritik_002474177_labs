176856e2b496322df9752ba1b2c5d349cf35779e
SinghHritik9919 <singh.hr@northeastern.edu>
2024-02-20 23:20:15 -0500

    Lab 04

---
+++ src/model/Product.java
@@ -0,0 +1,90 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author hriti
+ */
+public class Product {
+    
+    private String name;
+    private int price;
+    private int id;
+    
+    private ImageIcon logoImage;
+
+
+    
+    private ArrayList<Feature> features;
+
+    private static int count = 0;
+    
+    public Product() {
+        count++;
+        id = count;
+        
+        features = new ArrayList<Feature>();
+    }
+    
+    
+    public ImageIcon getLogoImage() {
+        return logoImage;
+    }
+
+    public void setLogoImage(ImageIcon logoImage) {
+        this.logoImage = logoImage;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+    
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public ArrayList<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(ArrayList<Feature> features) {
+        this.features = features;
+    }
+
+    public Feature addNewFeature() {
+        Feature newFeature = new Feature(this); // Passes the current product as owner
+        features.add(newFeature);
+        return newFeature;
+    }
+
+
+    
+    
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    
+    
+}
